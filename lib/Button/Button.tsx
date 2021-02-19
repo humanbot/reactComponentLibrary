2263a616b0dbe1089edbd877dea4023e943f3d1c
mkarmocha <mkarmocha@gmail.com>
2021-02-20 02:20:56 +1300

    updates

---
+++ lib/Button/Button.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export interface IButtonProps {
+    text:string
+}
+
+export default function Button (props: {text:string}) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
